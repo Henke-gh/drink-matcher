4fab1b9501f2192ec0b9b5673df8be6a9fbe7d96
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2025-04-10 21:26:40 +0200

    removed default styling

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="mainHeader">Drink Matcher</h1>
+      <img src="./cocktail_icon.png" alt="cocktail" className="cocktailImage" />
+    </header>
+  );
+}
