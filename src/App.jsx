0c79fe9612db91309483d064d2c144858474e193
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2025-04-16 10:08:58 +0200

    more card work

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,17 @@ function App() {
     <>
       <Header />
       <div className="presentation">
-      <h1>Welcome to the Drink Matcher</h1>
-        <p>We are thrilled to help you celebrate the most important moments of your life, one sip at a time. Whether you're looking for the perfect drink for your victory dance or just need a little liquid courage to escape the harsh realities of adulthood, we've got your back. After all, why stress when you can have a cocktail?</p>
-        <Footer />
+        <h1>Welcome to the Drink Matcher</h1>
+        <p>
+          We are thrilled to help you celebrate the most important moments of
+          your life, one sip at a time. Whether you're looking for the perfect
+          drink for your victory dance or just need a little liquid courage to
+          escape the harsh realities of adulthood, we've got your back. After
+          all, why stress when you can have a cocktail?
+        </p>
       </div>
+
+      <Footer />
     </>
   );
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,39 @@
 import "./Card.css";
 
-export default function Card() {
+export default function Card({ drink }) {
+  //loops through json response and extracts the ingredients and measures
+  //if there is no measure, returns only ingredient
+  //if ingredient has no value, breaks the loop
+  const ingredients = [];
+  for (let i = 1; i <= 15; i++) {
+    const ingredient = drink[`strIngredient${i}`];
+    const measure = drink[`strMeasure${i}`];
+    if (!ingredient) break;
+    ingredients.push(`${ingredient} ${measure || ""}`.trim());
+  }
   return (
     <div className="card">
-      <h2 className="cocktailHeader">Cocktail Name</h2>
+      <section className="cardHeader">
+        <img
+          className="cocktailImage"
+          src={drink?.strDrinkThumb || "./cocktail_icon.png"}
+          alt={drink?.strDrink || "Drink Name"}
+        />
+        <h2 className="cocktailHeader">{drink?.strDrink || "Drink Name"}</h2>
+      </section>
       <section className="cardContent">
         <div className="cardTextContainer">
           <p className="cardText bold">Ingredients</p>
+          <ul>
+            {ingredients.map((item, index) => (
+              <li key={index}>
+                <p className="cardText">{item}</p>
+              </li>
+            ))}
+          </ul>
           <p className="cardText bold">Instructions</p>
+          <p className="cardText">{drink?.strInstructions}</p>
         </div>
-        <img
-          src="./cocktail_icon.png"
-          alt="cocktail"
-          className="cocktailImage"
-        />
       </section>
     </div>
   );
