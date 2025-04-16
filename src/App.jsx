dfc1d7200ce8ccc1e962b6339abc66957f3019c6
Luisal182 <luisal.arranz@hotmail.com>
2025-04-16 10:12:25 +0200

    New components
    Action grid and Presentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import "./App.css";
+import ActionGrid from "./components/ActionGrid/ActionGrid";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Presentation from "./components/Presentation/Presentation";
 
 function App() {
   return (
     <>
       <Header />
-      <div className="presentation">
-      <h1>Welcome to the Drink Matcher</h1>
-        <p>We are thrilled to help you celebrate the most important moments of your life, one sip at a time. Whether you're looking for the perfect drink for your victory dance or just need a little liquid courage to escape the harsh realities of adulthood, we've got your back. After all, why stress when you can have a cocktail?</p>
+      <Presentation/>
+      <ActionGrid />
         <Footer />
-      </div>
+    
     </>
   );
 }
---
+++ src/components/Presentation/Presentation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Presentation.css";
+
+export default function Presentation() {
+    return (
+<div className="presentation">
+<h1>Welcome to the Drink Matcher</h1>
+  <p>We are thrilled to help you celebrate the most important moments of your life, one sip at a time. Whether you're looking for the perfect drink for your victory dance or just need a little liquid courage to escape the harsh realities of adulthood, we've got your back. After all, why stress when you can have a cocktail?</p>
+</div>
+  );
+}
